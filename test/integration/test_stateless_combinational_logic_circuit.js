84b917b9b7e4518c6801266a0f5659ad7ef41782
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-03-01 22:54:20 +0900

    Implement D flip-flop (test failed)

--- test/integration/test_stateless_combinational_logic_circuit.js
+++ test/integration/test_stateless_combinational_logic_circuit.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 var Prove = require('../prove.js');
 var createProve = Prove.createProve;
 var createSequentialProve = Prove.createSequentialProve;
@@ -15,6 +17,7 @@ var Or = logicGate.Or;
 var Not = logicGate.Not;
 var Buf = logicGate.Buf;
 var HazardFilter = logicGate.HazardFilter;
+var EdgeTrigger = logicGate.EdgeTrigger;
 
 describe('Stateless combinational logic circuits', function(){
   describe('Half adder', function(){
@@ -161,6 +164,180 @@ describe('Stateless combinational logic circuits', function(){
       // The output has hazards at intervals of 1 clock.
       // In this example, [ false, HAZARD, false, HAZARD, false, HAZARD, true ].
       and.pipe(new HazardFilter(1)).pipe(prove);
+
+      power.turnOn();
+    });
+  });
+
+
+
+  describe('D flip-flop', function() {
+    function DFlipFlop(inputE, inputD) {
+      var outputNotQ = new Buf();
+      var outputQ = new Buf();
+
+      var notD = inputD.pipe(new Not());
+
+      var and1 = new And();
+      inputE.pipe(and1);
+      notD.pipe(and1);
+
+      var and2 = new And();
+      inputE.pipe(and2);
+      inputD.pipe(and2);
+
+      var or1 = new Or();
+
+      var initiator = inputE.pipe(new InputHigh());
+      or1.on('data', _.once(function() {
+        initiator.unpipe(or1);
+        and1.pipe(new HazardFilter(1)).pipe(or1);
+
+        outputNotQ.pipe(new EdgeTrigger()).pipe(or1);
+      }));
+      initiator.pipe(or1);
+
+      var nor1 = or1.pipe(new Not());
+      nor1.pipe(outputQ);
+
+      var or2 = new Or();
+      and2.pipe(new HazardFilter(1)).pipe(or2);
+      outputQ.pipe(or2);
+
+      var nor2 = or2.pipe(new Not());
+      nor2.pipe(outputNotQ);
+
+      return { Q: outputQ };
+    }
+
+
+    it('should be able to set a true', function(done){
+      var power = new Power();
+      var clock = power.pipe(new Clock(1));
+
+      var inputSignalsE = [ true ];
+      var inputSignalsD = [ true ];
+
+      var inputE = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsE.shift();
+      }));
+      var inputD = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsD.shift();
+      }));
+
+      var ff = new DFlipFlop(inputE, inputD, power);
+
+      ff.Q.pipe(createSequentialProve([
+        /* initial state */ false,
+        /* set state     */ true,
+      ], done));
+
+      power.turnOn();
+    });
+
+
+    it('should be able to set a false', function(done){
+      var power = new Power();
+      var clock = power.pipe(new Clock(1));
+
+      var inputSignalsE = [ true ];
+      var inputSignalsD = [ false ];
+
+      var inputE = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsE.shift();
+      }));
+      var inputD = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsD.shift();
+      }));
+
+      var ff = new DFlipFlop(inputE, inputD, power);
+
+      ff.Q.pipe(createSequentialProve([
+        false, // initial state
+        false, // reset state
+      ], done));
+
+      power.turnOn();
+    });
+
+
+    it('should be able to store a true', function(done){
+      var power = new Power();
+      var clock = power.pipe(new Clock(2));
+
+      var inputSignalsE = [ true, false ];
+      var inputSignalsD = [ true, false ];
+
+      var inputE = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsE.shift();
+      }));
+      var inputD = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsD.shift();
+      }));
+
+      var ff = new DFlipFlop(inputE, inputD, power);
+
+      ff.Q.pipe(createSequentialProve([
+        false, // initial state
+        true, // set state
+        true, // stored state
+      ], done));
+
+      power.turnOn();
+    });
+
+
+    it('should be able to store a false', function(done){
+      var power = new Power();
+      var clock = power.pipe(new Clock(2));
+
+      var inputSignalsE = [ true, false ];
+      var inputSignalsD = [ false, false ];
+
+      var inputE = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsE.shift();
+      }));
+      var inputD = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsD.shift();
+      }));
+
+      var ff = new DFlipFlop(inputE, inputD, power);
+
+      ff.Q.pipe(createSequentialProve([
+        false, // initial state
+        false, // set state
+        false, // stored state
+      ], done));
+
+      power.turnOn();
+    });
+
+
+    it('should be able to store a false after storing a true', function(done){
+      var power = new Power();
+      var clock = power.pipe(new Clock(4));
+
+      var inputSignalsE = [ true, false, true, false ];
+      var inputSignalsD = [ true, false, false, false ];
+
+      var inputE = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsE.shift();
+      }));
+      var inputD = clock.pipe(new TimeSeriesInput(function() {
+        return inputSignalsD.shift();
+      }));
+
+      var ff = new DFlipFlop(inputE, inputD, power);
+
+      ff.Q.pipe(Prove.createLogProve('Q'));
+
+      ff.Q.pipe(createSequentialProve([
+        false, // initial state
+        true, // set state
+        true, // stored state
+        false, // reset state
+        false, // stored state
+      ], done));
 
       power.turnOn();
     });
